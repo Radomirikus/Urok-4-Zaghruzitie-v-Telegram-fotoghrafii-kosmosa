0300b19ec24f839fb3f6d9d1ed916b3ba4e22dff
Radomirikus <r2a3d0o3m1i2r@gmail.com>
2024-02-06 18:00:23 +0300

    название коммита

--- telegram_images_bot.py
+++ telegram_images_bot.py
@@ -1,14 +1,13 @@
 import telegram
+import os
+import random
 
 bot = telegram.Bot(token='6941078604:AAG0j6LkKrcbp4WNvJ-TmPk_fileOikl--Q')
-print(bot.get_me())
-bot.send_message(chat_id='@spaceimagesultra', text="""I put the new Forgis on the Jeep
-I trap until the bloody bottoms is underneath
-'Cause all my niggas got it out the streets
-I keep a hundred racks inside my jeans
-I remember hittin' the mall with the whole team
-Now a nigga can't answer calls 'cause I'm ballin'
-I was wakin' up, gettin' racks in the morning
-I was broke, now I'm rich, these niggas salty""")
-
-bot.send_document(chat_id='@spaceimagesultra', document=open('images/nasa_planet_image_0.png', 'rb'))
+# for rickroll in range(1):
+#     bot.send_message(chat_id='@spaceimagesultra', text="https://youtu.be/dQw4w9WgXcQ?si=4ZVhPJZQ3DMztKIh")
+directory_object = os.walk('images')
+for elements in directory_object:
+    folder, nested_folder, files = elements
+    random.shuffle(files)
+    for image in files:
+        bot.send_document(chat_id='@spaceimagesultra', document=open(f'images/{image}', 'rb'))
